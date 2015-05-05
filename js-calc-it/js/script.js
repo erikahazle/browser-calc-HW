47d7f6417432b0f1ef4c628b7cd8a5b7dd055ca7
Erika Jonikaite <erikajon@hotmail.co.uk>
2015-05-05 20:04:25 +0100

    Sorted number input and display

--- js-calc-it/js/script.js
+++ js-calc-it/js/script.js
@@ -1,5 +1,3 @@
-window.addEventListener("load", function() {
-
 var number = document.getElementsByClassName('number');
 var display = document.getElementById('display');
 
@@ -7,13 +5,24 @@ for(var i=0;i<number.length;i++){
   number[i].addEventListener('click', numberValue, false);
 }
 
+for(var i=0;i<number.length;i++){
+  operator[i].addEventListener('click', opValue, false);
+}
+
 function numberValue() {
   var attr = this.getAttribute('value');
-  display.addAttribute(attr);
+  var initialValue = display.getAttribute('value')
+  if (initialValue == null) {
+    initialValue = display.setAttribute('value', attr);
+    return initialValue;
+  } else {
+   initialValue = display.setAttribute('value', initialValue + attr);
+  }
+  return display.getAttribute('value');
 }
 
 var operator = document.getElementsByClassName('operator');
 
-})
+
 
 
